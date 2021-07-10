47ff1a6989d1ff4406f2f9d33e3a82793495bdab
dima <Dima.aleks.94@mail.ru>
2021-07-10 14:20:39 +0300

    two block + style + component skills/skill

--- Layout/src/main/Main.js
+++ Layout/src/main/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import style from './Main.module.css'
+import styleContainer from './../common/styles/Container.module.css'
+
 
 const Main = () =>  {
     return (
         <div className={style.mainBlock}>
-            <div className={style.container}>
+            <div className={styleContainer.container}>
                 <div className={style.text}>
                     <span>Hi There</span>
                     <h1>I am Dmitri Alekseonok</h1>
