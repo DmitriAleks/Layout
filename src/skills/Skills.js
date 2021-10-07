d6d57d9604c517a9f373c8e55f50c0475977cbb0
DmitriAleks <Dima.aleks.94@mail.ru>
2021-10-07 16:26:15 +0300

    created background for portfolio

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -3,8 +3,19 @@ import style from './Skills.module.css'
 import styleContainer from './../common/styles/Container.module.css'
 import Skill from "./skill/Skill";
 import Title from "../common/components/title/Title";
+import reactIcon from '../assets/image/reactIcon.png'
+
 
 const Skills = () => {
+
+        const test = {
+            color: 'blue',
+            backgroundImage: `url(${reactIcon})`,
+        }
+
+
+
+
     return (
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
@@ -12,7 +23,7 @@ const Skills = () => {
                 <div className={style.skills}>
                     <Skill title={'JS'} description={'Lorem ipsum dolor sit amet,'}/>
                     <Skill title={'CSS'} description={'sed do eiusmod tempor incididunt ut labore et dolore '}/>
-                    <Skill title={'React'}  description={' consectetur adipisicing elit, magna aliqua Ut enim'}/>
+                    <Skill title={'React'}  description={' consectetur adipisicing elit, magna aliqua Ut enim' } style={test}/>
                 </div>
             </div>
         </div>
